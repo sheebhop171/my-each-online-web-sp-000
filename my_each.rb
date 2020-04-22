28076ff2e52706cd6668cb5fe947cb1c7e07bdba
Shelby Hopkins <shelby.hopkins171@gmail.com>
2020-04-22 20:47:56 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,9 +1,14 @@
 def my_each(words)
   i = 0
-  
+
   while i < words.length
       yield (words[i])
       i = i + 1
     end
 
+end
+
+collection = [1, 2, 3, 4]
+my_each(collection) do |i|
+  puts i
 end
